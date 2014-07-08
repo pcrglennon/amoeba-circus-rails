ea44154bf2d466ae5c32c3742005a940e01dc599
pcrglennon <pcrglennon@gmail.com>
2014-07-08 15:56:21 -0400

    Init Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmoebaCircusRails::Application.config.secret_key_base = 'd712f6f9e98066e8cec976f1b4dccbdadf42e70b1139fab6e08cece4c40b56d938f3d6aae76901877127424ece614504e7e612ca8705d9643518734bf968a18d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmoebaCircusRails::Application.config.session_store :cookie_store, key: '_amoeba-circus-rails_session'
