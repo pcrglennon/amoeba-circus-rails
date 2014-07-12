c9de4974f1fb27fb4cbf32c7cb1d678de1a7fb4a
pcrglennon <pcrglennon@gmail.com>
2014-07-12 18:08:58 -0400

    Add talent_name to Amoeba model, which returns 'None' if amoeba has no talent

--- app/models/amoeba.rb
+++ app/models/amoeba.rb
@@ -3,6 +3,10 @@ class Amoeba < ActiveRecord::Base
   has_many :acts_amoebas
   has_many :acts, :through => :acts_amoebas
 
+  def talent_name
+    self.talent ? self.talent.name : "None"
+  end
+
   def split
     Amoeba.create(:name => "#{name} C1", :generation => generation + 1, :talent => talent, :acts => acts)
     Amoeba.create(:name => "#{name} C2", :generation => generation + 1, :talent => talent, :acts => acts)
